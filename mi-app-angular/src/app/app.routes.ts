5d1d4ba9c11d488a9f16bc23b08e2c9f6cfdad40
Cristhian Nuñez Bonilla <thecrisss@icloud.com>
2026-05-28 08:05:43 +0000

    menu centrado

--- mi-app-angular/src/app/app.routes.ts
+++ mi-app-angular/src/app/app.routes.ts
@@ -7,9 +7,9 @@ import { Pregunta2Component } from './pages/pregunta2/pregunta2.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'pregunta1', component: Pregunta1Component },
+  { path: 'pregunta2', component: Pregunta2Component },
   { path: 'inicio', component: InicioComponent },
   { path: 'productos', component: ProductosComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: 'pregunta1', component: Pregunta1Component },
-  { path: 'pregunta2', component: Pregunta2Component },
 ];
